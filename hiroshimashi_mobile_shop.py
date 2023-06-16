f5e1256fb413ee103fd01037379f24bcac0cc1f1
fuminishiki <136785393+fuminishiki@users.noreply.github.com>
2023-06-16 18:28:57 +0900

    Create hiroshimashi_mobile_shop.py

---
+++ hiroshimashi_mobile_shop.py
@@ -0,0 +1,288 @@
+import folium
+
+m = folium.Map(location=[34.45,132.4],zoom_start=11)
+m
+
+folium.Marker(location=[34.3735528,132.3952746],popup='ソフトバンク アルパーク店　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4447035,132.4618206],popup='ソフトバンク イオンモール広島祇園店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3940516,132.4996475],popup='ソフトバンク イオンモール広島府中店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3869626,132.4310426],popup='ソフトバンク 観音店　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3757973,132.4629615],popup='ソフトバンク ゆめタウン広島店　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4725657,132.4527777],popup='ソフトバンク+ワイモバイル 相田店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3831243,132.3561015],popup='ソフトバンク+ワイモバイル 五日市城山店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3729286,132.3578995],popup='ソフトバンク+ワイモバイル 五日市中央店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3964286,132.4736805],popup='ソフトバンク+ワイモバイル エールエール広島駅前店　　　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.5212126,132.5069542],popup='ソフトバンク+ワイモバイル 可部バイパス店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3877144,132.4183812],popup='ソフトバンク+ワイモバイル 庚午店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4652942,132.5039043],popup='ソフトバンク+ワイモバイル 高陽中央店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3868563,132.3583469],popup='ソフトバンク+ワイモバイル サンリブ五日市店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4096785,132.3978264],popup='ソフトバンク+ワイモバイル ジアウトレット広島店　　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4674877,132.4227516],popup='ソフトバンク+ワイモバイル 伴東店　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3953936,132.4552827],popup='ソフトバンク+ワイモバイル 広島紙屋町店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3853896,132.4757858],popup='ソフトバンク+ワイモバイル 広島段原ショッピングセンター店　　　　　　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3923165,132.4608546],popup='ソフトバンク+ワイモバイル 広島本通商店街店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4079544,132.503487],popup='ソフトバンク+ワイモバイル フォレオ広島東店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3860921,132.4643314],popup='ソフトバンク+ワイモバイル フジグラン広島店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4662505,132.4769080],popup='ソフトバンク+ワイモバイル フジグラン緑井店　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3934876,132.4579139],popup='ソフトバンク+ワイモバイル 本通駅前店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.5229643,132.5123688],popup='ソフトバンク+ワイモバイル マルナカ可部店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3583956,132.4565678],popup='ソフトバンク+ワイモバイル ゆめタウンみゆき店　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.4098933,132.4502081],popup='ソフトバンク+ワイモバイル 横川駅店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3728448,132.406712],popup='ソフトバンク+ワイモバイル LECT広島T-SITE店　　　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="gray")).add_to(m)
+m
+
+folium.Marker(location=[34.3736671,132.3952220],popup='ドコモ アルパーク店　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4445143,132.4617344],popup='ドコモ イオンモール広島祇園店　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3937631,132.4994152],popup='ドコモ イオンモール広島府中店　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.385476,132.3561581],popup='ドコモ 五日市城山店　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3645948,132.3622636],popup='ドコモ 五日市宮島街道店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3622123,132.4592689],popup='ドコモ 宇品ベイエリア店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3872407,132.4949116],popup='ドコモ 大州店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4096482,132.4489678],popup='ドコモ カジル横川店　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.522182,132.5129991],popup='ドコモ 可部中央店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4371984,132.4730141],popup='ドコモ 祇園新道店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4655742,132.5044489],popup='ドコモ 高陽店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4468168,132.3966157],popup='ドコモ 西風新都店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3860809,132.4174696],popup='ドコモ 西広島店　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3853860,132.4758281],popup='ドコモ 広島段原ショッピングセンター店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.39998332,132.4789338],popup='ドコモ 広島光町店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3924572,132.4602554],popup='ドコモ 広島本通店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3851256,132.442636],popup='ドコモ 舟入店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3935336,132.4566525],popup='ドコモ 本通駅前店　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3736476,132.471749],popup='ドコモ 翠町店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4733606,132.4896612],popup='ドコモ 八木店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4739717,132.4552438],popup='ドコモ 安東店　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.3759194,132.4630581],popup='ドコモ ゆめタウン広島店　　　　　　　　　　　',icon=folium.Icon(color="red")).add_to(m)
+m
+
+folium.Marker(location=[34.4791069,132.4922174],popup='au トヨタカローラ広島(株)北支店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3841554,132.4154412],popup='au トヨタカローラ広島(株)西支店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.390733,132.4501287],popup='au トヨタカローラ広島(株)広島支店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4035719,132.4475023],popup='au 広島トヨタ自動車(株)本店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3985108,132.5037622],popup='au+UQモバイル 安芸府中店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3735288,132.3951846],popup='au+UQモバイル アルパーク店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3625776,132.4683256],popup='au+UQモバイル イオン宇品店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4446089,132.4617775],popup='au+UQモバイル イオンモール広島祇園店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3941454,132.4994547],popup='au+UQモバイル イオンモール広島府中店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3705155,132.3570263],popup='au+UQモバイル 五日市コイン通り店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3847426,132.356254],popup='au+UQモバイル 五日市城山店　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3871964,132.4936441],popup='au+UQモバイル 大州通り店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4626283,132.4683675],popup='au+UQモバイル 大町店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3864187,132.4319277],popup='au+UQモバイル 観音店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3846447,132.416063],popup='au+UQモバイル 庚午店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4767556,132.5212638],popup='au+UQモバイル 高陽店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3881332,132.4767418],popup='au+UQモバイル 段原店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4437638,132.4799861],popup='au+UQモバイル 東原店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3778086,132.4790967],popup='au+UQモバイル 広島トヨペット(株)霞店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3936036,132.4418112],popup='au+UQモバイル 広島トヨペット(株)観音店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.495746,132.5077114],popup='au+UQモバイル 広島トヨペット(株)八木店　　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3771706,132.3680498],popup='au+UQモバイル 広島トヨペット五日市店　　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.467458,132.4181135],popup='au+UQモバイル 広島沼田店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4756187,132.4902409],popup='au+UQモバイル 広島八木店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4663528,132.4764108],popup='au+UQモバイル フジグラン緑井店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3932938,132.4574657],popup='au+UQモバイル 本通駅前店　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.5232660,132.5118943],popup='au+UQモバイル マルナカ可部店　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3758584,132.4630098],popup='au+UQモバイル ゆめタウン広島店　　　　　　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.4098933,132.4502081],popup='au+UQモバイル 横川店　　　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3929395,132.4587338],popup='au+UQモバイル HIROSHIMA　　　　　　　　',icon=folium.Icon(color="orange")).add_to(m)
+m
+
+folium.Marker(location=[34.3735407,132.3950041],popup='ワイモバイル アルパーク店　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.4446805,132.4618803],popup='ワイモバイル イオンモール広島祇園店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.3944241,132.4998520],popup='ワイモバイル イオンモール広島府中店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.4747901,132.4880032],popup='ワイモバイル 緑井店　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.3834531,132.4295352],popup='ワイモバイル 南観音店　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.3766779,132.4635496],popup='ワイモバイル ゆめタウン広島店　　　　　　　　　　　　　　',icon=folium.Icon(color="lightred")).add_to(m)
+m
+
+folium.Marker(location=[34.4439600,132.4617877],popup='UQモバイル イオンモール広島祇園店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="lightblue")).add_to(m)
+m
+
+folium.Marker(location=[34.3950933,132.4995970],popup='UQモバイル イオンモール広島府中店　　　　　　　　　　　　　　　　',icon=folium.Icon(color="lightblue")).add_to(m)
+m
+
+folium.Marker(location=[34.3754885,132.4632482],popup='UQモバイル ゆめタウン広島店　　　　　　　　　　　　　',icon=folium.Icon(color="lightblue")).add_to(m)
+m
+
+folium.Marker(location=[34.4446045,132.4614454],popup='楽天モバイル イオンモール広島祇園店　　　　　　　　　　　　　　　　　',icon=folium.Icon(color="pink")).add_to(m)
+m
+
+folium.Marker(location=[34.3934121,132.4574509],popup='楽天モバイル 広島本通り店　　　　　　　　　　　　',icon=folium.Icon(color="pink")).add_to(m)
+m
+
+folium.Marker(location=[34.3756611,132.4637688],popup='楽天モバイル ゆめタウン広島店　　　　　　　　　　　　　　',icon=folium.Icon(color="pink")).add_to(m)
+m
+
+folium.Marker(location=[34.4427442,132.4611678],popup='イオンモバイル 広島祇園店　　　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.3953374,132.4986208],popup='イオンモバイル 広島府中店　　　　　　　　　　　　',icon=folium.Icon(color="purple")).add_to(m)
+m
+
+folium.Marker(location=[34.3736269,132.3959372],popup='mineo 広島店　　　　　　',icon=folium.Icon(color="green")).add_to(m)
+m
+
+folium.Marker(location=[34.3975903,132.4735216],popup='マーキュリー 中四国支店　　　　　　　　　　　',icon=folium.Icon(color="white")).add_to(m)
+m
+
+m.save('hiroshimashi_mobile_shop.html')
